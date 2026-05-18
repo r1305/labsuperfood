a20b65de749067e65d7e2334418557e5444889df
Rogger Aburto <rogger.aburto@gmail.com>
2026-05-18 16:37:08 -0500

    fix: configurar timezone -05:00 (America/Lima) en conexión MySQL

--- database.js
+++ database.js
@@ -5,7 +5,8 @@ const dbConfig = {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  timezone: '-05:00'
 };
 
 async function createConnection() {
